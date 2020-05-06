6ea24644e921222eb445f5d72f780af691953a82
jorge bejarano <jorge.bejarano@yahoo.com>
2020-05-06 12:12:26 -0400

    add put method to app.js

--- Client/app.js
+++ Client/app.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
  $("#getMovieList").append(function(e){
             $("#getMovieListResult").val("");
             $.ajax({
@@ -35,9 +35,24 @@
              }
            });
         });
-          
-=======
 
+        function MoviesList(){
+            jQuery.statusText;
+            var data = {
+                movieName: ("name").value(),
+                title: ("title").value(),
+                director: ("director").value(),
+                genre: ("genre").value()
+            };
+
+            $.ajax({
+                contentType: "application/json",
+                type: 'put',
+                url: "https://localhost:44325/api/movie",
+                data: JSON,
+            })
+        }
+          
 $(function(){
     $.get("https://localhost:44325/api/movie", function(data){
         console.log(data);
@@ -49,31 +64,3 @@ $(function(){
 })
 
 
-(function($){
-    function processForm( e ){
-        var dict = {
-        	Title : this["title"].value,
-            Director: this["director"].value,
-            Genre: this["genre"].value
-        };
-
-        $.ajax({
-            url: 'https://localhost:44325/api/movie',
-            dataType: 'json',
-            type: 'post',
-            contentType: 'application/json',
-            data: JSON.stringify(dict),
-            success: function( data, textStatus, jQxhr ){
-                $('#response pre').html( data );
-            },
-            error: function( jqXhr, textStatus, errorThrown ){
-                console.log( errorThrown );
-            }
-        });
-
-        e.preventDefault();
-    }
-
-    $('#my-form').submit( processForm );
-})(jQuery);
->>>>>>> cdbdffc9b8c7ca94086ef9ac9bc985da0648e0ff
